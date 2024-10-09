06b273ec04df7f8968567a61baebe41fd77844ee
llddang <bodyness@naver.com>
2024-10-10 01:18:49 +0900

    feat: 회원가입 페이지 구현

---
+++ WebGL Builds/webgl/src/page/signUp/script.js
@@ -0,0 +1,10 @@
+const signInForm = document.getElementById("sign-form");
+
+const nicknameInput = document.getElementById("nickname");
+const passwordInput = document.getElementById("password");
+
+signInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // TODO: 회원가입 api 연결
+});
