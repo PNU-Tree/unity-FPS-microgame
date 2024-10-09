1fe7f963b54b349afdde2e527d6f70d28f1572b9
llddang <bodyness@naver.com>
2024-10-10 01:18:27 +0900

    feat: 페이지 제목 구현

---
+++ WebGL Builds/webgl/src/components/pageTitle.js
@@ -0,0 +1,25 @@
+class PageTitle extends HTMLElement {
+  static get observedAttributes() {
+    return ["title"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "title") {
+      this.render();
+    }
+  }
+
+  render() {
+    const title = this.getAttribute("title") || "Title";
+    this.innerHTML = `
+      <div style="font-size: 1.5rem; padding-bottom: 2rem; cursor: default;">
+        <a href="/" style="padding-right: 0.5rem">
+          <i class="fa-solid fa-arrow-left"></i>
+        </a>
+        ${title}
+      </div>
+    `;
+  }
+}
+
+customElements.define("page-title", PageTitle);
